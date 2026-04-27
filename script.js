9630fbef6c8a4e16bd2017722825bcb24c9a098e
Mariejoe Yaun <mariejoeyaun14@gmail.com>
2026-04-27 18:54:01 +0800

    Add new skill feature

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ const logForm = document.getElementById("logForm");
 const logSkillSelect = document.getElementById("logSkill");
 const skillsContainer = document.getElementById("skillsContainer");
 
+skillForm.addEventListener("submit", function(e) {
+
+}
